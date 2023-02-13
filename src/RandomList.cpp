0967b83c153fec333a133bfda547f92a8fc27e83
韵 <98337638+CQJTU-Linzd@users.noreply.github.com>
2023-02-13 23:31:35 +0800

    Update RandomList.cpp

--- src/RandomList.cpp
+++ src/RandomList.cpp
@@ -8,24 +8,35 @@ public:
     }
 };
 
-// 生成长度为size，数值范围在l~r上的链表
-ListNode* randomList(int size, int l, int r) {
+void test() {
     srand(time(0));
-    ListNode* head = new ListNode(rand() % (r - l + 1) + l);
-    ListNode* cur = head;
-    for (int i = 0; i < size; i++) {
-        int val = rand() % (r - l + 1) + l;
-        cur->next = new ListNode(val);
-        cur = cur->next;
+    // 需要在对数器里生成链表
+    // 不能单独写一个randomList函数，否则得到的链表全都一样
+    for (int test = 0; test < 100; test++) { // 对数器 测100次
+        // generateList
+        ListNode* head = new ListNode(rand() % (100 - 0 + 1) + 0);
+        ListNode* cur = head;
+        int size = rand() % (100 - 1 + 1) + 1;
+        for (int i = 1; i < size; i++) { //链表长度为size
+            cur->next = new ListNode(rand() % (100 - 0 + 1) + 0);
+            cur = cur->next;
+        }
+
+        // printList
+        while (head) {
+            cout << head->val << " ";
+            head = head->next;
+        }
+        cout << endl;
+        
+        // 释放内存
+        delete head;
+        head = NULL;
     }
-    return head;
+    cout << "end" << endl;
 }
 
 int main() {
-    ListNode* head = randomList(10, 0, 1000);
-    while (head) {
-        cout << head->val << " ";
-        head = head->next;
-    }
+    test();
     return 0;
 }
