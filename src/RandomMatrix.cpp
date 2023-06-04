9c08fa9671e415f4394fe57061a69dbb4fddc35a
韵 <98337638+CQJTU-Linzd@users.noreply.github.com>
2023-06-04 17:55:16 +0800

    Update RandomMatrix.cpp

--- src/RandomMatrix.cpp
+++ src/RandomMatrix.cpp
@@ -3,15 +3,13 @@ using namespace std;
 #include<ctime>
 #include<vector>
 
-// 生成1~maxrow行，1~maxcol列，数据范围在l~r的随机矩阵
-vector<vector<int>> randomMatrix(int maxrow, int maxcol, int l, int r) {
+// 生成n行，m列，数据范围在l~r的随机矩阵
+vector<vector<int>> randomMatrix(int n, int m, int l, int r) {
     srand(time(0));
-    int n = rand() % maxrow + 1;
-    int m = rand() % maxcol + 1;
     vector<vector<int>>matrix(n, vector<int>(m));
-    for (vector<int>& nums : matrix) {
-        for (int& num : nums) {
-            num = rand() % (r - l + 1) + l;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            matrix[i][j] = rand() % (r - l + 1) + l;
         }
     }
     return matrix;
