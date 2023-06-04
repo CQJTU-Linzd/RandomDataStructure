62e6f2ec0d1ce553ea2c1a87d16432158ff9d85c
韵 <98337638+CQJTU-Linzd@users.noreply.github.com>
2023-06-04 17:52:57 +0800

    Update RandomAnyCharaterString.cpp

--- src/RandomAnyCharaterString.cpp
+++ src/RandomAnyCharaterString.cpp
@@ -3,7 +3,7 @@ using namespace std;
 #include<ctime>
 #include<string>
 
-// 生成大小为size，包含任意字符的字符串
+// 生成大小为size，包含任意字符的随机字符串
 string randomAnyCharacterString(int size) {
     srand(time(0));
     string s = "";
